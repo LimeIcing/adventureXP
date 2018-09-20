78f5b130cadb49151240ccd6d3d3eef9a8bb8c0f
ultracake <jackievuong@live.dk>
2018-09-20 15:32:41 +0200

    h

--- src/main/java/mandatory/axp/Models/employ_Model.java
+++ src/main/java/mandatory/axp/Models/employ_Model.java
@@ -1,4 +1,19 @@
 package src.main.java.mandatory.axp.Models;
 
-public class employ_Model {
+public class employ_model
+{
+    //variabler
+    private String name;
+    private String password;
+
+    //constructors
+    public employ_model()
+    {
+
+    }
+
+    public employ_model(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
