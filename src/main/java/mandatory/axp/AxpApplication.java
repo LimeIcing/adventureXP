76ea4dbd326dbee0181458440c8ae02dd482175c
Lime Icing <emil577b@stud.kea.dk>
2018-09-20 14:53:55 +0200

    created project

---
+++ src/main/java/mandatory/axp/AxpApplication.java
@@ -0,0 +1,12 @@
+package mandatory.axp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxpApplication.class, args);
+    }
+}
