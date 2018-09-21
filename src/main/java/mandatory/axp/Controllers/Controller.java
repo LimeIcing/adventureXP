3884747e1aba9fa39b139f878db7d93ede3c0219
Lime Icing <emil577b@stud.kea.dk>
2018-09-21 09:31:35 +0200

    Cleaned up the controller(s)

--- src/main/java/mandatory/axp/Controllers/Controller.java
+++ src/main/java/mandatory/axp/Controllers/Controller.java
@@ -1,6 +1,16 @@
 package mandatory.axp.Controllers;
 
 public class Controller {
-
-
+    //Missing data from project, wait to continued!!!
+    public boolean loginMethod(String username, String password) {
+        boolean login = false;
+        //secret code
+        String sUsername = "admin", sPassword = "admin";
+        
+        //checks if username and password is correct
+        if (sUsername.equals(username) && sPassword.equals(password)) {
+            login = true;
+        }
+        return login;
+    }
 }
