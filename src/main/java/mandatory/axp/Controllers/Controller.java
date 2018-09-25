5765893cdcaa4d3818918bbdcf11dade2de70c8e
Simon <simo390m@stud.kea.dk>
2018-09-25 10:44:36 +0200

    Her er lagt en udvidet controller klasse med forskellige indexes på HTML sider

--- src/main/java/mandatory/axp/Controllers/Controller.java
+++ src/main/java/mandatory/axp/Controllers/Controller.java
@@ -1,6 +1,11 @@
 package mandatory.axp.Controllers;
 
+import mandatory.axp.Models.ActivityModel;
+import mandatory.axp.Models.Repositories.ActivityRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -17,10 +22,33 @@ public class Controller {
         return login;
     }
 
+    private ActivityRepository activityRepository = new ActivityRepository();
+
+    @RequestMapping("/")
+    public String homePage()
+    {
+        return "index";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String createActivity(Model model)
+    {
+        model.addAttribute("activity", new ActivityModel());
+
+        return "create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String createActivity(@ModelAttribute ActivityModel activityModel)
+    {
+        activityRepository.create(activityModel);
+
+        return "index";
+    }
+
     @RequestMapping("/activities")
     public String activitiesPage()
     {
-
 
         return "activities";
     }
