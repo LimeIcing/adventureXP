6ad552aee900b3bf7529b514bfd6d6ea4628568f
Lime Icing <emil577b@stud.kea.dk>
2018-09-27 13:52:22 +0200

    cleaned up code

--- src/main/java/mandatory/axp/Models/Repositories/ActivityRepository.java
+++ src/main/java/mandatory/axp/Models/Repositories/ActivityRepository.java
@@ -10,28 +10,31 @@ public class ActivityRepository {
     private String sql;
 
     public void create(ActivityModel activityModel) {
-        sql = "insert into Activity(name, duration, minAge, minHeight, price) values('" + activityModel.getName() + "'" +
-                ", '" + activityModel.getDurationMin() + "' " +
-                ", '" + activityModel.getMinAge() + "' " +
-                ", '" + activityModel.getMinHeightCm() + "' " +
-                ", '" + activityModel.getPrice() +  "')";
+        sql = "insert into Activity(name, duration, minAge, minHeight, price) values('" +
+                activityModel.getName() + "', '" +
+                activityModel.getDurationMin() + "', '" +
+                activityModel.getMinAge() + "', '" +
+                activityModel.getMinHeightCm() + "', '" +
+                activityModel.getPrice() +  "')";
 
         jdbc.update(sql);
     }
 
     public void update(ActivityModel activityModel, int id) {
-        sql = "update Activity " +
-                "set name = '" +  activityModel.getName() + "', duration = '"
-                + activityModel.getDurationMin() + "', minAge = '" + activityModel.getMinAge() + "', minHeight = '"
-                + activityModel.getMinHeightCm() + "', price = '" + activityModel.getPrice() + "' where activityId = " + id;
+        sql = "update Activity set name = '" +
+                activityModel.getName() + "', duration = '" +
+                activityModel.getDurationMin() + "', minAge = '" +
+                activityModel.getMinAge() + "', minHeight = '" +
+                activityModel.getMinHeightCm() + "', price = '" +
+                activityModel.getPrice() +
+                "' where activityId = " + id;
 
         jdbc.update(sql);
     }
 
 
     public void delete(int id) {
-        sql = "delete from Activity " +
-                "where activityId = " + id;
+        sql = "delete from Activity where activityId = " + id;
 
         jdbc.update(sql);
     }
