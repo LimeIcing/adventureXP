b745f7ceebd2d2ac994cd4f226cbf1c934a2be42
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-09-27 14:44:53 +0200

    missing dataSourcse set

--- src/main/java/mandatory/axp/Controllers/AdminController.java
+++ src/main/java/mandatory/axp/Controllers/AdminController.java
@@ -2,19 +2,26 @@ package mandatory.axp.Controllers;
 
 import mandatory.axp.Models.ActivityModel;
 import mandatory.axp.Models.Repositories.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+
+
 @Controller
 public class AdminController {
 
-    @RequestMapping(value = "/admin/students", method = RequestMethod.GET)
-    public String adminStudents (Model model) {
-        model.addAttribute("students", ActivityRepository.());
+    @Autowired
+    private ActivityRepository AR = new ActivityRepository();
 
-        return "/admin/students";
+    @RequestMapping(value = "/admin/admin", method = RequestMethod.GET)
+    public String adminStudents (Model model) {
+        model.addAttribute("activities", AR.getActivities());
+
+        return "/admin/admin";
     }
 }
