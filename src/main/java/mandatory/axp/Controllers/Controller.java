62912dfa85f6e539b769e33536c572d9dd66b0b0
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-10-04 13:26:14 +0200

    made an activity details page

--- src/main/java/mandatory/axp/Controllers/Controller.java
+++ src/main/java/mandatory/axp/Controllers/Controller.java
@@ -5,6 +5,7 @@ import mandatory.axp.Models.Repositories.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,10 +55,10 @@ public class Controller {
         return "/activities";
     }
 
-    @RequestMapping(value = "/activity", method = RequestMethod.GET)
-    public String activityPage()
+    @RequestMapping(value = "/activity/{id}", method = RequestMethod.GET)
+    public String activityPage(@PathVariable(value = "id") int id, Model model)
     {
-
+        model.addAttribute("activity", AR.getSpecificActivityModelBasedOnId(id));
         return "/activity";
     }
 /*
