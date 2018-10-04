f7dd7aaa9a7b1b46b87410018768f097bfa94b97
Lime Icing <emil577b@stud.kea.dk>
2018-10-04 14:16:23 +0200

    cleaned up

--- src/main/java/mandatory/axp/Models/BookingModel.java
+++ src/main/java/mandatory/axp/Models/BookingModel.java
@@ -10,44 +10,41 @@ public class BookingModel {
     private Date date;
 
     public BookingModel(int bookingID, int activityID, int customerID, Date date) {
-
         this.bookingID = bookingID;
         this.activityID = activityID;
         this.customerID = customerID;
         this.date = date;
     }
 
-    //setters
-    public void setBookingID(int bookingID) {
-        this.bookingID = bookingID;
-    }
-
-    public void setActivityID(int activityID) {
-        this.activityID = activityID;
-    }
-
-    public void setCustomerID(int customerID) {
-        this.customerID = customerID;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    //getters
     public int getBookingID() {
         return bookingID;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
     }
 
     public int getActivityID() {
         return activityID;
     }
 
+    public void setActivityID(int activityID) {
+        this.activityID = activityID;
+    }
+
     public int getCustomerID() {
         return customerID;
     }
 
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
     public Date getDate() {
         return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
