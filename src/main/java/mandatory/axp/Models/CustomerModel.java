8bd54bf8d6c44d9888b56dbf89fae1b8b06a74c7
Lasse Jon <lasss@live.dk>
2018-10-04 13:32:34 +0200

    Har ændret userModel til customerModel har lavet bookingRepo, som ikke er færdigt

--- src/main/java/mandatory/axp/Models/CustomerModel.java
+++ src/main/java/mandatory/axp/Models/CustomerModel.java
@@ -3,14 +3,30 @@ package mandatory.axp.Models;
 public class CustomerModel {
 
     private String name;
-    private String ID;
+    private int ID;
     private String password;
 
-    public CustomerModel(String name, String ID, String password) {
+    public CustomerModel(String name, int ID, String password) {
 
         this.name = name;
         this.ID = ID;
         this.password = password;
     }
 
+    public CustomerModel(String ID) {
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
